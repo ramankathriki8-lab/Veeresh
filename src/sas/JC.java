95e7dccfd7c340a423ce02a56a967f65b1b3d955
ramankathriki8-lab <ramankathriki8@gmail.com>
2025-12-30 08:27:33 +0530

    Update JC.java

--- src/sas/JC.java
+++ src/sas/JC.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 public class JC {
 	public static void main(String[] args) {
-
 		int[] arr = { 5, 2, 9, 1, 6 };
-
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] < arr[j]) {
