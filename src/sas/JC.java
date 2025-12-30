7bf9d59a0d9a65da510c1b10a67baacbed4550d4
ramankathriki8-lab <ramankathriki8@gmail.com>
2025-12-30 08:23:57 +0530

    Update JC.java

--- src/sas/JC.java
+++ src/sas/JC.java
@@ -1,11 +1,8 @@
 package sas;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class JC {
-
 	public static void main(String[] args) {
 
 		int[] arr = { 5, 2, 9, 1, 6 };
