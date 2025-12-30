ab8d4de3bb5f4bfd0b1dc5fbfe69727bbc107e0b
RAM <kathriraman8@gmail.com>
2025-12-30 07:36:39 +0530

    new updated things

--- src/sas/JC.java
+++ src/sas/JC.java
@@ -1,42 +1,27 @@
 package sas;
 
-import java.util	.Arrays;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class JC {
 
 	public static void main(String[] args) {
-		
 
-		int[] arr = {5, 2, 9, 1, 6};
+		int[] arr = { 5, 2, 9, 1, 6 };
 
 		for (int i = 0; i < arr.length; i++) {
-		    for (int j = i + 1; j < arr.length; j++) {
-		        if (arr[i] < arr[j]) {
-		            int temp = arr[i];
-		            arr[i] = arr[j];
-		            arr[j] = temp;
-		        }
-		    }
-		}
-
-/////////////////////dsa
-
-	
-			    //git status 
-		
-			//kathrik raman
-
-
-		for (int i : arr) {
-		    System.out.print(i + " ");
-		}
-
-			
-			///djjd
-				
-				
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
 			}
-		
 		}
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
